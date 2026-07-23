34f5574f72276f61ddc762b668106fc1e79a64ca
big4-Admin <big4tiles@gmail.com>
2026-07-24 01:52:43 +0530

    fix(web): mark sitemap as force-dynamic to prevent build-time DB execution

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -2,6 +2,9 @@ import { MetadataRoute } from "next";
 import { siteConfig } from "@/lib/config/site";
 import { getCategories, getProducts } from "@/lib/api";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = siteConfig.website.replace(/\/$/, "");
 
